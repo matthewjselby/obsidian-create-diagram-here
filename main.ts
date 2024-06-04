d962d4833774b4b7ae1ea8767b989eaaed6de7a4
Matt Selby <matthewjselby@gmail.com>
2024-06-04 21:59:10 +0000

    add settings item for SVG template and style settings pane

--- main.ts
+++ main.ts
@@ -1,15 +1,7 @@
-import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting, moment } from 'obsidian';
 import { exec } from 'child_process';
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting, moment } from 'obsidian';
 
-interface CreateImageHerePluginSettings {
-	pathToImageEditor: string;
-}
-
-const DEFAULT_SETTINGS: CreateImageHerePluginSettings = {
-	pathToImageEditor: ''
-}
-
-const newSVGText = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+const defaultNewSVGText = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!-- Created with Inkscape (http://www.inkscape.org/) -->
 
 <svg
@@ -24,139 +16,178 @@ const newSVGText = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns="http://www.w3.org/2000/svg"
    xmlns:svg="http://www.w3.org/2000/svg">
-  <sodipodi:namedview
-	 id="namedview1"
-	 pagecolor="#505050"
-	 bordercolor="#eeeeee"
-	 borderopacity="1"
-	 inkscape:showpageshadow="0"
-	 inkscape:pageopacity="0"
-	 inkscape:pagecheckerboard="0"
-	 inkscape:deskcolor="#505050"
-	 inkscape:document-units="mm"
-	 inkscape:zoom="0.78751403"
-	 inkscape:cx="397.45324"
-	 inkscape:cy="553.64093"
-	 inkscape:window-width="1392"
-	 inkscape:window-height="1027"
-	 inkscape:window-x="0"
-	 inkscape:window-y="25"
-	 inkscape:window-maximized="0"
-	 inkscape:current-layer="layer1" />
-  <defs
-	 id="defs1" />
-  <g
-	 inkscape:label="Layer 1"
-	 inkscape:groupmode="layer"
-	 id="layer1" />
+   <style>
+        @media (prefers-color-scheme: light) {
+            svg {
+                filter: invert(1);
+            }
+        }
+    </style>
+    <sodipodi:namedview
+        id="namedview1"
+        pagecolor="#505050"
+        bordercolor="#eeeeee"
+        borderopacity="1"
+        inkscape:showpageshadow="0"
+        inkscape:pageopacity="0"
+        inkscape:pagecheckerboard="0"
+        inkscape:deskcolor="#505050"
+        inkscape:document-units="mm"
+        inkscape:zoom="0.78751403"
+        inkscape:cx="397.45324"
+        inkscape:cy="553.64093"
+        inkscape:window-width="1392"
+        inkscape:window-height="1027"
+        inkscape:window-x="0"
+        inkscape:window-y="25"
+        inkscape:window-maximized="0"
+        inkscape:current-layer="layer1" />
+    <defs
+        id="defs1" />
+    <g
+        inkscape:label="Layer 1"
+        inkscape:groupmode="layer"
+        id="layer1" />
 </svg>`
 
-export default class CreateImageHerePlugin extends Plugin {
-	settings: CreateImageHerePluginSettings;
+interface CreateImageHerePluginSettings {
+    pathToImageEditor: string;
+    newSVGTemplate: string;
+}
 
-	editImage(imagePath: string) {
-		if (this.app.vault.adapter instanceof FileSystemAdapter) {
-			const imageFullPath = this.app.vault.adapter.getFullPath(imagePath);
-			exec(`open "${imageFullPath}"`, (error, stdout, sterr) => {
+const DEFAULT_SETTINGS: CreateImageHerePluginSettings = {
+    pathToImageEditor: '',
+    newSVGTemplate: defaultNewSVGText
+}
+
+export default class CreateImageHerePlugin extends Plugin {
+    settings: CreateImageHerePluginSettings;
+
+    editImage(imagePath: string) {
+        if (this.app.vault.adapter instanceof FileSystemAdapter) {
+            const imageFullPath = this.app.vault.adapter.getFullPath(imagePath);
+            exec(`open "${imageFullPath}"`, (error, stdout, sterr) => {
 				if (error) {
 					console.error(`Error opening SVG: ${error}`);
 					return;
 				}
 			});
-		}
-	}
+        }
+    }
 
-	async createAndOpenImage() {
-		if (this.app.workspace.activeEditor?.editor != null) {
-			const pathForSVG = await this.app.fileManager.getAvailablePathForAttachment(
-				`New image ${moment().format('YYYYMMDDHHmmss')}.svg`
-			);
+    async createAndOpenImage() {
+        if (this.app.workspace.activeEditor?.editor != null) {
+            const pathForSVG = await this.app.fileManager.getAvailablePathForAttachment(
+                `New image ${moment().format('YYYYMMDDHHmmss')}.svg`
+            );
 
-			await this.app.vault.create(pathForSVG, newSVGText);
-			this.app.workspace.activeEditor?.editor?.replaceSelection(`![[${pathForSVG}]]`);
+            await this.app.vault.create(pathForSVG, this.settings.newSVGTemplate);
+            this.app.workspace.activeEditor?.editor?.replaceSelection(`![[${pathForSVG}]]`);
 
-			this.editImage(pathForSVG);
-		}
-	}
+            this.editImage(pathForSVG);
+        }
+    }
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// Adds a command to create a new image at the cursor and open it for editing
-		this.addCommand({
-			id: 'create-image-here',
-			name: 'Create image here',
-			callback: async () => {
-				this.createAndOpenImage();
-			}
-		});
+        // Adds a command to create a new image at the cursor and open it for editing
+        this.addCommand({
+            id: 'create-image-here',
+            name: 'Create image here',
+            callback: async () => {
+                this.createAndOpenImage();
+            }
+        });
 
-		this.registerEvent(
-			this.app.workspace.on("editor-menu", (menu, editor, view) => {
-				menu.addItem((item) => {
-					item
-						.setTitle("Create image here 🩻")
-						.setIcon("image")
-						.onClick(async () => {
-							this.createAndOpenImage();
-						});
-				});
-			})
-		);
+        this.registerEvent(
+            this.app.workspace.on("editor-menu", (menu, editor, view) => {
+                menu.addItem((item) => {
+                    item
+                        .setTitle("Create diagram here 📈")
+                        .setIcon("image")
+                        .onClick(async () => {
+                            this.createAndOpenImage();
+                        });
+                });
+            })
+        );
 
-		this.registerEvent(
-			this.app.workspace.on("file-menu", (menu, file) => {
-				menu.addItem((item) => {
-					item
-						.setTitle("Open image for editing 📝")
-						.setIcon("document")
-						.setDisabled(!file.path.endsWith(".svg"))
-						.onClick(async () => {
-							this.editImage(file.path);
-						});
-				});
-			})
-		);
+        this.registerEvent(
+            this.app.workspace.on("file-menu", (menu, file) => {
+                menu.addItem((item) => {
+                    item
+                        .setTitle("Open diagram for editing 📝")
+                        .setIcon("document")
+                        .setDisabled(!file.path.endsWith(".svg"))
+                        .onClick(async () => {
+                            this.editImage(file.path);
+                        });
+                });
+            })
+        );
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new CreateImageHereSettingTab(this.app, this));
-	}
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new CreateImageHereSettingTab(this.app, this));
+    }
 
-	onunload() {
+    onunload() {
 
-	}
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class CreateImageHereSettingTab extends PluginSettingTab {
-	plugin: CreateImageHerePlugin;
+    plugin: CreateImageHerePlugin;
 
-	constructor(app: App, plugin: CreateImageHerePlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: CreateImageHerePlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Path to Image Editor')
-			.setDesc('Absolute path to desired image editor on your computer. If left empty, the default image editor will be used.')
-			.addText(text => text
-				.setPlaceholder('')
-				.setValue(this.plugin.settings.pathToImageEditor)
-				.onChange(async (value) => {
-					this.plugin.settings.pathToImageEditor = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Path to Image Editor')
+            .setDesc('Absolute path to desired image editor on your computer. If left empty, the default image editor for SVGs will be used.')
+            .setClass('block-setting-item')
+            .addText(text => text
+                .setValue(this.plugin.settings.pathToImageEditor)
+                .onChange(async (value) => {
+                    this.plugin.settings.pathToImageEditor = value;
+                    await this.plugin.saveSettings();
+                })
+                .inputEl.setCssProps({
+                    "width": "100%",
+                    "margin-top": "1em"
+                })
+            );
+
+        new Setting(containerEl)
+            .setName('SVG Template')
+            .setDesc('Template to use when creating a new diagram.')
+            .setClass('block-setting-item')
+            .addTextArea(textArea => textArea
+                .setValue(this.plugin.settings.newSVGTemplate)
+                .onChange(async (value) => {
+                    this.plugin.settings.newSVGTemplate = value;
+                    await this.plugin.saveSettings();
+                })
+                .inputEl.setCssProps({
+                    "width": "100%",
+                    "height": "350px",
+                    "margin-top": "1em"
+                })
+            );
+    }
 }
